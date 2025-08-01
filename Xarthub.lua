090a58cf19e21a797c1c627075989a88f37a1140
cozxmoc0-cmd <cozxmoc0@gmail.com>
2025-08-01 23:58:50 +0700

    Update Xarthub.lua

--- Xarthub.lua
+++ Xarthub.lua
@@ -1,50 +1,49 @@
--- Xart Hub by Coz | Inspired by FischMain
+-- Xart Hub by Coz | Compatible with Delta X
 
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source', true))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Xart Hub",
-   LoadingTitle = "Xart Executor",
-   LoadingSubtitle = "Initializing Script...",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = "XartHub",
-      FileName = "XartHubConfig"
-   },
-   KeySystem = false
+    Name = "Xart Hub",
+    LoadingTitle = "Xart Executor",
+    LoadingSubtitle = "Initializing Script...",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "XartHub",
+        FileName = "XartHubConfig",
+    },
+    KeySystem = false
 })
 
 local Tab = Window:CreateTab("Auto", 4483362458)
 
 Tab:CreateToggle({
-   Name = "Auto Fish",
-   CurrentValue = false,
-   Flag = "XartAutoFish",
-   Callback = function(Value)
-      getgenv().autoFish = Value
-      while getgenv().autoFish do
-         print("🎣 Xart: Fishing...")
-         wait(1.5)
-      end
-   end
+    Name = "Auto Fish",
+    CurrentValue = false,
+    Flag = "XartAutoFish",
+    Callback = function(Value)
+        getgenv().autoFish = Value
+        while getgenv().autoFish do
+            print("🎣 Xart: Fishing...")
+            wait(1.5)
+        end
+    end
 })
 
 Tab:CreateToggle({
-   Name = "Auto Sell",
-   CurrentValue = false,
-   Flag = "XartAutoSell",
-   Callback = function(Value)
-      getgenv().autoSell = Value
-      while getgenv().autoSell do
-         print("💰 Xart: Selling fish...")
-         wait(2)
-      end
-   end
+    Name = "Auto Sell",
+    CurrentValue = false,
+    Flag = "XartAutoSell",
+    Callback = function(Value)
+        getgenv().autoSell = Value
+        while getgenv().autoSell do
+            print("💰 Xart: Selling fish...")
+            wait(2)
+        end
+    end
 })
 
 local CreditTab = Window:CreateTab("Credits", 4483362458)
-
 CreditTab:CreateParagraph({
-   Title = "Xart Hub",
-   Content = "Script by Coz\nInspired by FischMain"
+    Title = "Xart Hub",
+    Content = "Script by Coz\nInspired by FischMain"
 })
